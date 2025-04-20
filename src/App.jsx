37878c599994eb5d87993fbb3b137816177bf081
hemang200 <ihemangsingh@gmail.com>
2025-04-20 22:59:32 +0530

    initilize commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import ReactDOM from "react-dom/client";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="text-5xl text-red-800 font-bold text-center">hellow</div>
+    </>
+  )
+}
+
+export default App
