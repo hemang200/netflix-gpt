ff124bbe9a6f072d12f1eddb998d8770739f056b
HEMANG KR SINGH <166155338+hemang200@users.noreply.github.com>
2025-04-27 03:42:57 +0530

    Update constants.jsx

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -8,8 +8,7 @@ export const USER_AVATAR =
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjFjNzRlNjc5YmVjNzIzYjRmMTE3ZWY1YTA2OTc3YiIsIm5iZiI6MTc0NTUwMTQxOS42NjksInN1YiI6IjY4MGEzY2ViMTMwZjc2ZWIzYjlkMTRiZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._AIR0DNtFE7wi3GvZcxLHyZB1CSDkeoJpnnHIfwFTPM'
-    }
+      Authorization: ''    }
   };
 
  export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
